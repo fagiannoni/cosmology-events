07c1dd5bf2c493fad586bbaacab8491ce34ed443
Franco Giannoni <franco.giannoni.h@gmail.com>
2023-02-20 17:29:46 -0800

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <iframe
+          id="timelineFrame"
+          title="timelineFrame"
+          src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1_B74NGR8TBTo0I0N8pgvAv5LwBgnZ-GXPWIoMjEN9bk&font=Default&lang=en&start_at_end=true&initial_zoom=0&height=650'
+          width='100%'
+          height='650'
+          allowFullScreen={true}
+        />
+        <div className="right small">
+          <a className='no-link-color' href="https://www.flaticon.com/free-icons/universe" title="universe icon">Favicon by Freepik - Flaticon</a>
+        </div>
       </header>
     </div>
   );
